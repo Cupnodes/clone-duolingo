b9b0941a05b5bc51c76044ee103aa0f17b528b5b
cupnodes <aldairgomesdecastrosales@gmail.com>
2025-06-16 12:06:38 -0300

    Added Buttons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Nunito } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+const nunitoFont = Nunito({
+  subsets: ['latin']
 });
 
 export const metadata: Metadata = {
@@ -25,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${nunitoFont} ${nunitoFont} antialiased`}
       >
         {children}
       </body>
